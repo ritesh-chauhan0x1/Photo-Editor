3608ff00d4aa3495874ef77e4a4317c3698c6495
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:32 +0530

    Initial commit: Photo-Editor

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "Photo-Editor"</h1>
+        <p>"Image manipulation tool"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
